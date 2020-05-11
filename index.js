0c0336d24a813e6bfa89561baab5396618c36138
kksrini89 <kksrini89@gmail.com>
2020-05-11 12:44:09 +0530

    Design styles are updated.

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
- :host { }
- ::slotted(*) { }
- .highlight {
-   color: blue;
- }
-.search-form {
-  position: relative;
-}
 .input-control {
-  padding: 3px;
-  border: none;
-  background-color: #fff;
-  color: #373737;
-  font-weight: bold;
+  border-radius: 5px;
+  color: #333;
+  padding: 8px;
+  outline: none;
 }
 .search-results {
   list-style-type: none;
   display: none;
+  padding: 5px;
+  width: 50%;
+  margin: 5px;
+}
+
+.search-results li {
+  padding: 5px;
 }
 </style>
 <div class="container">
   <input class="input-control" type="text" id="searchInputId" name="searchInput" required />
-  <button class="btn" id="searchBtnId">Search</button>
   <ul class="search-results"></ul>
 </div>
 `;
@@ -33,11 +30,21 @@ class SearchInputComponent extends HTMLElement {
     super();
     this._shadowRoot = this.attachShadow({ mode: 'open' });
   }
-  
+
+  get displayField() {
+    return this._displayField;
+  }
+
+  set displayField(val) {
+    if (!!val) {
+      this._displayField = val;
+    }
+  }
+
   get list() {
     return this.hasAttribute('list');
   }
-  
+
   set list(val) {
     if (val) {
       this.setAttribute('list', val);
@@ -45,38 +52,47 @@ class SearchInputComponent extends HTMLElement {
       this.removeAttribute('list');
     }
   }
-  
+
   static get observedAttributes() {
-    return ['list'];
+    return ['list', 'displayField'];
   }
-  
+
   connectedCallback() {
     this._shadowRoot.appendChild(template.content.cloneNode(true));
-    // this.resultsContainer.style.display = 'none';
-    
+
     // DOM element references
     this.searchInput = this._shadowRoot.querySelector('#searchInputId');
     this.searchBtn = this._shadowRoot.querySelector('#searchBtnId');
     this.resultsContainer = this._shadowRoot.querySelector('.search-results');
     this.searchInput.addEventListener('input', this.onSearch.bind(this));
     this._list = [];
+    this._displayField = '';
   }
 
   attributeChangedCallback(name, oldVal, newVal) {
     if (name === 'list') {
       this._list = [...JSON.parse(newVal)];
     }
+
+    if (name === 'displayField' && !!newVal) {
+      this._displayField = newVal;
+    }
   }
 
   /**
    * To search element
-   * @param {Event} event 
+   * @param {Event} event
    */
   onSearch(event) {
     const text = event.target.value;
     if (text !== '') {
       if (this._list && this._list.length) {
-        this.bindFilteredUser(this._list.filter((data) => data.name.includes(text)));
+        const filteredValues = this._list.filter((data) => {
+          if (data && !!this.displayField) {
+            return data[this._displayField].toLowerCase().includes(text.toLowerCase());
+          }
+        });
+        filteredValues && filteredValues.length && this.bindFilteredUser(filteredValues);
       }
     } else {
       this.collapseListElement();
@@ -88,8 +104,8 @@ class SearchInputComponent extends HTMLElement {
     if (arr && arr.length) {
       console.log(arr);
       this.resultsContainer.innerHTML = '';
-      for (const { name } of arr) {
-        const li = this.bindResultToHtml(name);
+      for (const item of arr) {
+        const li = this.bindResultToHtml(item[this._displayField]);
         this.resultsContainer.appendChild(li);
       }
       this.resultsContainer.style.display = 'block';
@@ -98,16 +114,17 @@ class SearchInputComponent extends HTMLElement {
 
   /**
    * To bind result with Results Container
-   * @param {*} val 
+   * @param {*} val
    */
   bindResultToHtml(val) {
-    const li = document.createElement('li');
-    const text = document.createTextNode(val);
-    li.appendChild(text);
-    return li;
+    if (!!val) {
+      const li = document.createElement('li');
+      const text = document.createTextNode(val);
+      li.appendChild(text);
+      return li;      
+    }
   }
 
-  
   /**
    * To collapse results container element
    */
